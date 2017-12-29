b1685b9f60fe2e3e30cd4ee135fc5cdf18df6d01
zaqwer101 <platonov256@gmail.com>
2017-12-29 04:25:54 +0000

    Немного исправлений

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@ kernel:
 	gcc -m32 -c kernel.c -o kc.o
 	ld -m elf_i386 -T link.ld -o kernel kasm.o kc.o
 clean:
-	rm -rf kasm.o kc.o kernel
+	rm -rf kasm.o kc.o kernel.o
--- kernel.c
+++ kernel.c
@@ -1,24 +1,34 @@
 void kmain(void)
 {
 	const char *str = "Hello, my little dude";
- 	char *vidptr = (char*)0xb8000; // Тут начинается видеопамять 
+ 	char *screen = (char*)0xb8000; // Тут начинается видеопамять 
  	unsigned int i = 0;
  	unsigned int j = 0;
  
  	while(j < 80 * 25 * 2) {	// Магия очистки экрана 
-		vidptr[j] = ' ';	// Символ, который вносим в участок
-		vidptr[j+1] = 0x07; 	// Атрибуты символа
+		screen[j] = ' ';		// Символ, который вносим в участок
+		screen[j+1] = 0x07; 	// Атрибуты символа
 		j = j + 2;
 	}
  	
 	j = 0;
  	
 	while(str[j] != '\0') {
- 		vidptr[i] = str[j];	// То же, что и с очисткой, только заносим части строки
-	 	vidptr[i+1] = 0x03;
+ 		screen[i] = str[j];		// То же, что и с очисткой, только заносим части строки
+	 	screen[i+1] = 0x03;
  		++j;
  		i = i + 2;
  	}
  	
 	return;
+}
+
+void clean()
+{
+		
+}
+
+void print()
+{
+	
 }
